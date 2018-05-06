b71154bc535e60ed713fd0e71f1c176169b012a2
Pulimet <korolev2009@gmail.com>
2018-05-06 15:37:07 +0300

    Add animations test

--- app/src/main/java/net/alexandroid/lottietest/MainActivity.java
+++ app/src/main/java/net/alexandroid/lottietest/MainActivity.java
@@ -3,11 +3,19 @@ package net.alexandroid.lottietest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LottieAnimationView lottieAnimationView =  findViewById(R.id.animation_view);
+        lottieAnimationView.setImageAssetsFolder("assets/");
+        lottieAnimationView.setAnimation("data.json");
+        lottieAnimationView.loop(true);
+        lottieAnimationView.playAnimation();
     }
 }
